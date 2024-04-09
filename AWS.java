e496d16eae063257383040ff0b83bb61118fa65e
Eddrick Liu <eddrickliu@yahoo.com>
2024-04-08 21:14:32 -0700

    AWS questions

---
+++ AWS.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class AWS {
+    public static void main(String[] args){
+
+    }
+
+    /**
+     * https://leetcode.com/problems/minimum-adjacent-swaps-to-make-a-valid-array/description/
+     * 
+     */
+    public static int getMinSwaps(List<Integer> blocks){
+        return 0;
+    }
+
+    /**
+     * determine the most amount of negatives that can will allow the cumulative sum to still be positive
+     * 
+     */
+    public static int getMaxNegatives(List<Integer> PnL){
+        return 0;
+    }
+}
