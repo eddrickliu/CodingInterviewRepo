607bd81399781371bda6764d523df82958e7b0aa
Eddrick Liu <eddrickliu@yahoo.com>
2024-04-29 19:52:00 -0700

    start adaptiva

---
+++ Adaptiva.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Adaptiva {
+    public static void main(String[] args){
+        Integer[] test1 = {3,2,1,23,4,12};
+        Integer[] test2 = {2,1,1,12,2,2};
+        Integer[] test3 = {6,5,3,1};
+        // System.out.println("getMinSwaps(test1)"+getMinSwaps(Arrays.asList(test1)));
+        // System.out.println("getMinSwaps(test2)"+getMinSwaps(Arrays.asList(test2)));
+        // System.out.println("getMinSwaps(test3)"+getMinSwaps(Arrays.asList(test3)));
+        // System.out.println("getMaxNegatives(test1)"+getMaxNegatives(Arrays.asList(test1)));
+        // System.out.println("getMaxNegatives(test2)"+getMaxNegatives(Arrays.asList(test2)));
+        // System.out.println("getMaxNegatives(test3)"+getMaxNegatives(Arrays.asList(test3)));
+    }
+
+    
+}
