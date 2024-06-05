9e3438bf12ed9f6ca0b47483c7a1bfdb4596cd96
Eddrick Liu <eddrickliu@yahoo.com>
2024-06-04 19:28:10 -0700

    start amazon VOS notes

---
+++ AWSVirtualOnsite.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class AWSVirtualOnsite {
+        public static void main(String[] args){
+        Integer[] test1 = {3,2,1,23,4,12};
+        Integer[] test2 = {2,1,1,12,2,2};
+        Integer[] test3 = {6,5,3,1};
+        // System.out.println("getMinSwaps(test1)"+getMinSwaps(Arrays.asList(test1)));
+        // System.out.println("getMinSwaps(test2)"+getMinSwaps(Arrays.asList(test2)));
+        // System.out.println("getMinSwaps(test3)"+getMinSwaps(Arrays.asList(test3)));
+        // System.out.println("getMaxNegatives(test1)"+getMaxNegatives(Arrays.asList(test1)));
+        // System.out.println("getMaxNegatives(test2)"+getMaxNegatives(Arrays.asList(test2)));
+        // System.out.println("getMaxNegatives(test3)"+getMaxNegatives(Arrays.asList(test3)));
+    }
+
+    /**
+     * https://leetcode.com/problems/analyze-user-website-visit-pattern/
+     * 
+     */
+    public static List<String> mostVisitedPattern(String[] username, int[] timestamp, String[] website) {
+        return null;
+    }
+
+    /**
+     * https://leetcode.com/problems/find-the-celebrity/description/
+     * 
+     */
+    public static int findCelebrity(int PnL){
+        return 0;
+    }
+}
